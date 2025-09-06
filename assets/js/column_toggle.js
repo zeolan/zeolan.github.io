f8c2cc1a14daf1d6bc34344ee860a2b251d14677
zeolan <zealan74@gmail.com>
2025-09-06 20:20:37 +0300

    Fixed console errors

--- assets/js/column_toggle.js
+++ assets/js/column_toggle.js
@@ -1,34 +1,39 @@
 (function () {
   document.addEventListener("DOMContentLoaded", function () {
-    document
-      .getElementById("toggle-ro")
-      .addEventListener("change", function () {
+    var toggle_ro_element = document.getElementById("toggle-ro");
+    if (toggle_ro_element) {
+      // Check if the element exists
+      toggle_ro_element.addEventListener("change", function () {
         var opacity = this.checked ? "1" : "0";
         document.querySelectorAll(".col-ro").forEach(function (cell) {
           cell.style.opacity = opacity;
         });
       });
-    document
-      .getElementById("toggle-ua")
-      .addEventListener("change", function () {
-        var opacity = this.checked ? "1" : "0";
-        document.querySelectorAll(".col-ua").forEach(function (cell) {
-          cell.style.opacity = opacity;
+      document.querySelectorAll(".col-ro").forEach(function (cell) {
+        cell.style.opacity = "1";
+        cell.addEventListener("click", function () {
+          var opacity = this.style.opacity == "1" ? "0" : "1";
+          this.style.opacity = opacity;
         });
       });
-    document.querySelectorAll(".col-ro").forEach(function (cell) {
-      cell.style.opacity = "1";
-      cell.addEventListener("click", function () {
-        var opacity = this.style.opacity == "1" ? "0" : "1";
-        this.style.opacity = opacity;
+    }
+    var toggle_ua_element = document.getElementById("toggle-ua");
+    if (toggle_ua_element) {
+      document
+        .getElementById("toggle-ua")
+        .addEventListener("change", function () {
+          var opacity = this.checked ? "1" : "0";
+          document.querySelectorAll(".col-ua").forEach(function (cell) {
+            cell.style.opacity = opacity;
+          });
+        });
+      document.querySelectorAll(".col-ua").forEach(function (cell) {
+        cell.style.opacity = "1";
+        cell.addEventListener("click", function () {
+          var opacity = this.style.opacity == "1" ? "0" : "1";
+          this.style.opacity = opacity;
+        });
       });
-    });
-    document.querySelectorAll(".col-ua").forEach(function (cell) {
-      cell.style.opacity = "1";
-      cell.addEventListener("click", function () {
-        var opacity = this.style.opacity == "1" ? "0" : "1";
-        this.style.opacity = opacity;
-      });
-    });
+    }
   });
 })();
